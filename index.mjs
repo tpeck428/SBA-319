30e55623c33ba79fbaa1fcc095efa2b9ada73103
tpeck428 <toripeck73@gmail.com>
2024-04-06 22:04:59 -0400

    inital files, express set up and server working

---
+++ index.mjs
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT;
+const app = express();
+
+
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+    res.send("Welcome to the API.");
+});
+
+
+// Global error handling
+app.use((err, _req, res, next) => {
+    res.status(500).send("seems like we messed up somewhere.")
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+})
